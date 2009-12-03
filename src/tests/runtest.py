0962296725a905dc8933890e4bad7399ccb47047
Kristian Klette <klette@klette.us>
2009-12-03 13:05:52 +0100

    Added tests for pyroutes.http. 100% code coverage on the core stuff! :-)

--- src/tests/runtest.py
+++ src/tests/runtest.py
@@ -3,4 +3,5 @@ from CoverageTestRunner import CoverageTestRunner
 r = CoverageTestRunner()
 
 r.add_pair("pyroutes/__init__.py", "tests/routestest.py")
+r.add_pair("pyroutes/http.py", "tests/httptest.py")
 r.run()
