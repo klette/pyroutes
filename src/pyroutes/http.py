8e7c4a66cf7dc990877b53a02b678c6021f85b31
xim <xim@akuma.no>
2009-12-25 08:56:52 +0800

    Fixes header variable somehow surviving across Response instances

--- src/pyroutes/http.py
+++ src/pyroutes/http.py
@@ -12,17 +12,17 @@ class Response:
     a content, headers and status_code parameter.
     headers should be passed in as a tuple.
     """
-    def __init__(self, content=None, headers=[('Content-Type', 'text/html; charset=utf8')], status_code='200 OK'):
+    def __init__(self, content=None, headers=None, status_code='200 OK'):
         self.content = content
-        self.headers = headers
-        self.status_code  = status_code
-        
+        self.headers = headers or [('Content-Type', 'text/html; charset=utf8')]
+        self.status_code = status_code
+
 
 class Redirect(Response):
     """
     A redirect shortcut class for redirection responses
     """
-    
+
     def __init__(self, location):
         self.content = "redirect"
         self.headers = [('Location', location)]
