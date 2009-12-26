98cfe40a6beb7dcf28f9c68e1dcdf925554ac8cc
xim <xim@akuma.no>
2009-12-26 22:39:44 +0100

    Added settings.py for holding default and importing custom settings

---
+++ src/pyroutes/settings.py
@@ -0,0 +1,16 @@
+import os
+
+"""Used for importing settings, and defining default settings"""
+
+# Default template dir, relative to this imported __file__
+BUILTIN_TEMPLATES_DIR = os.path.join(
+    os.path.dirname(__file__),
+    'default_templates'
+)
+BUILTIN_BASE_TEMPLATE = os.path.join(BUILTIN_TEMPLATES_DIR, 'base.xml')
+
+# Attempt to get custom settings. Not obligatory.
+try:
+    from pyroutes_settings import *
+except ImportError:
+    pass
