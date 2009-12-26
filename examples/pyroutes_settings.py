14b53daed28eb5de043336af7d58a1fba961b61c
xim <xim@akuma.no>
2009-12-26 22:41:08 +0100

    Example file with settings. Not needed for the example project, and should be documented otherwise as well

---
+++ examples/pyroutes_settings.py
@@ -0,0 +1,10 @@
+# Path to media files
+#DEV_MEDIA_BASE = '.'
+
+# Used for custom HttpException.
+# Passed variables are request and title.
+# Defining only page templates and not base template is allowed.
+#CUSTOM_BASE_TEMPLATE = './templates/pyroutes/base_override.xml'
+#403_TEMPLATE = './templates/403.xml'
+#404_TEMPLATE = './templates/404.xml'
+#500_TEMPLATE = './templates/500.xml'
