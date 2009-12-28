60e7e0d4361d2acdd7274e135dfc9ed0f959e3c9
xim <xim-git@akuma.no>
2009-12-28 07:44:52 +0100

    make the tests run regardless of cwd

--- src/tests/runtest.py
+++ src/tests/runtest.py
@@ -1,13 +1,21 @@
 import sys
-from os import path
+from os import path, chdir
 
 from CoverageTestRunner import CoverageTestRunner
 
-sys.path.insert(0, path.join(path.dirname(__file__), '..'))
-
 r = CoverageTestRunner()
 
-r.add_pair("pyroutes/__init__.py", "tests/routestest.py")
-r.add_pair("pyroutes/http.py", "tests/httptest.py")
-r.add_pair("pyroutes/utils.py", "tests/utilstest.py")
+base_dir = path.abspath(path.join(path.dirname(__file__), '..'))
+chdir(base_dir)
+sys.path.insert(0, base_dir)
+
+mapping = (
+    ('__init__.py', 'routestest.py'),
+    ('http.py', 'httptest.py'),
+    ('utils.py', 'utilstest.py')
+)
+
+for pair in mapping:
+    r.add_pair(path.join('pyroutes', pair[0]), path.join('tests', pair[1]))
+
 r.run()
