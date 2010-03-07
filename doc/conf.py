d245672069706edc9916189843c6611d48effb5c
Kristian <klette@samfundet.no>
2010-03-07 18:09:48 +0100

    Include documentation for private members (thanks to Jodal)

--- doc/conf.py
+++ doc/conf.py
@@ -17,13 +17,15 @@ import sys, os
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
 sys.path.append(os.path.abspath(os.path.dirname(__file__ + '../src')))
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
 
 # General configuration
 # ---------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'autodoc_private_members']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
