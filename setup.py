93693a0c158bde0ecf01f7d090a776521e04ecf5
Kristian <klette@samfundet.no>
2010-04-12 20:01:10 +0200

    Fix various build issues

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from setuptools import setup, find_packages
+from distutils.core import setup
 import os
 
 setup(
@@ -10,16 +10,13 @@ setup(
     url = 'http://github.com/klette/pyroutes',
     license = 'GPLv2',
     description = "A small WSGI wrapper for creating small python web apps",
-    long_description = open(os.path.join(os.path.dirname(__file__), 'README.markdown')).read(),
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
     author = 'Kristian Klette',
     author_email = 'klette@samfundet.no',
-    packages = find_packages('src'),
+    packages = ['pyroutes','pyroutes.http', 'pyroutes.template', 'pyroutes.contrib'],
     package_dir = {'': 'src'},
-    install_requires = ['setuptools'],
     requires = ['wsgiref'],
     package_data = {'pyroutes': ['default_templates/*.xml', 'default_templates/fileserver/*.xml']},
-    zip_ok=False,
-    zip_safe=False,
 
     classifiers = [
         'Development Status :: 3 - Alpha',
