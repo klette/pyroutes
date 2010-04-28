5ed203716f3b820a3529217d0d930ede495c965e
Kristian <klette@samfundet.no>
2010-04-28 16:53:38 +0200

    Remove unused extensions

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'autodoc_private_members']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
