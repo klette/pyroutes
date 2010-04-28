7b7550ac7a7499233bcf15f47b7c388d67b0efdf
Kristian <klette@samfundet.no>
2010-04-28 16:47:59 +0200

    Docs are for development branch when not released

--- doc/conf.py
+++ doc/conf.py
@@ -44,9 +44,9 @@ copyright = '2009, Kristian Klette'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.2.2'
+version = 'development branch'
 # The full version, including alpha/beta/rc tags.
-release = '0.2.2'
+release = 'development branch'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
