b85a99c00a5c55b771a74fcfd387b01902e67d10
Kristian <klette@samfundet.no>
2010-05-02 13:37:12 +0200

    Added middleware to setup.py and up to 0.2.3-rc1

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ from distutils.core import setup
 import os
 
 setup(
-    name = "pyroutes",
-    version = "0.2.2",
+    name = "pyroutes-daily",
+    version = "0.2.3-rc1",
     url = 'http://github.com/klette/pyroutes',
     license = 'GPLv2',
     description = "A small WSGI wrapper for creating small python web apps",
     long_description = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     author = 'Kristian Klette',
     author_email = 'klette@samfundet.no',
-    packages = ['pyroutes','pyroutes.http', 'pyroutes.template', 'pyroutes.contrib'],
+    packages = ['pyroutes','pyroutes.http', 'pyroutes.template', 'pyroutes.contrib', 'pyroutes.middleware'],
     requires = ['wsgiref'],
     package_data = {'pyroutes': ['default_templates/*.xml', 'default_templates/fileserver/*.xml']},
 
