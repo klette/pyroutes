a98801ae41fae68557ca02b0b552c72ffb48b7ee
eide <eide@samfundet.no>
2010-05-03 17:20:47 +0200

    Specify from_list in middleware import statement

--- pyroutes/__init__.py
+++ pyroutes/__init__.py
@@ -110,7 +110,7 @@ def application(environ, start_response):
         module_name = full_path[:last_dot]
         class_name = full_path[last_dot + 1:]
 
-        module = __import__(module_name)
+        module = __import__(module_name, globals(), locals(), [class_name], -1)
         middleware = getattr(module, class_name)
 
         chain = middleware(chain)
