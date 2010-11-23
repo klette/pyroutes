5e896844c054296cae9c142b6d83083f6fdc65a1
xim <xim-git@akuma.no>
2010-11-23 02:01:36 -0300

    Make an some tests for the dispatcher

--- tests/dispatcher_test.py
+++ tests/dispatcher_test.py
@@ -1,6 +1,10 @@
 import unittest
 import pyroutes
 from pyroutes.dispatcher import Dispatcher
+from pyroutes.http.request import Request
+from pyroutes.http.response import Response
+from pyroutes.route import Route
+import pyroutes.settings as settings
 
 class TestDispatcher(unittest.TestCase):
 
@@ -28,3 +32,34 @@ class TestDispatcher(unittest.TestCase):
         self.assertTrue(dispatcher.find_request_handler('/baz/param') != None)
         self.assertTrue(dispatcher.find_request_handler('/class') != None)
         self.assertTrue(dispatcher.find_request_handler('') != None)
+
+    def test_dispatch(self):
+        args_given = [None, None]
+        class ArgKeep(object):
+            def __init__(self, *args, **kwargs):
+                args_given[0] = args
+                args_given[1] = kwargs
+
+        def do_test(handler, path, result):
+            environment = {'PATH_INFO': path}
+            handler = pyroutes.route(path)(handler)
+            response = pyroutes.dispatcher.dispatch(environment, ArgKeep)
+            self.assertTrue(isinstance(handler, Route))
+            self.assertEquals(response, [result])
+            self.assertEquals(args_given, [('200 OK', [('Content-Type', 'text/html; charset=utf-8')]), {}])
+
+        do_test(lambda x: Response('result'), '/response1', 'result')
+        do_test(lambda x: Response(['result']), '/response2', 'result')
+
+    def test_middleware_chainer(self):
+        handler = lambda x: 'result'
+        request = Request({})
+
+        self.assertEquals(settings.MIDDLEWARE,
+                ('pyroutes.middleware.errors.NotFoundMiddleware',
+                'pyroutes.middleware.errors.ErrorHandlerMiddleware'))
+
+        handler = pyroutes.route('/path')(handler)
+        result = pyroutes.dispatcher.create_middleware_chain(handler, request)
+        self.assertTrue(isinstance(handler, Route))
+        self.assertEquals(result, 'result')
