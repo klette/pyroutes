861530509721bcf2f53a39e9ab3a918282d28878
xim <xim-git@akuma.no>
2011-04-15 03:50:40 -0300

    Update the example to show the power of allowing args to a @route('/')

--- examples/wiki.py
+++ examples/wiki.py
@@ -13,24 +13,24 @@ renderer = TemplateRenderer("templates/base.xml")
 nodes = {}
 
 @route('/')
-def main(request):
-    return Redirect('/show/index')
+def main(request, page='index'):
+    return Redirect('/show/%s' % page)
 
 @route('/edit')
-def edit(request,node):
+def edit(request, node='index'):
     if 'new_node_data' in request.POST:
         nodes[node] = request.POST['new_node_data']
         return Redirect('/show/%s' % node)
 
     template_data = {
         # XML-Template will remove the textarea node if None
-        '#edit_contents': nodes.get(node) or '',
+        '#edit_contents': nodes.get(node, ''),
         '#edit_form/action': '/edit/%s' % node,
     }
     return Response(renderer.render("templates/edit.xml", template_data), status_code="404 Not Found")
 
 @route('/show')
-def show(request, node):
+def show(request, node='index'):
     node_contents = nodes.get(node)
 
     if node_contents is None:
