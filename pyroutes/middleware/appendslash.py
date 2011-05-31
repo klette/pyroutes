9ef845cc794ecfc91b1419419a8382b7c47615b6
xim <xim-git@akuma.no>
2011-05-30 21:23:06 -0300

    Make it class style, like the other middlewares

--- pyroutes/middleware/appendslash.py
+++ pyroutes/middleware/appendslash.py
@@ -1,12 +1,14 @@
 from pyroutes.http.response import Redirect
 
-def AppendSlashes(passthrough, route):
+class AppendSlashes(object):
     """
     Used for adding slashes at the end of all URLs. i.e. GET /foo/bar will
     return a redirect to /foo/bar/
     """
-    def appendslash(request):
+    def __init__(self, passthrough, route):
+        self.passthrough = passthrough
+
+    def __call__(self, request):
         if not request.ENV['PATH_INFO'].endswith('/'):
             return Redirect(request.ENV['PATH_INFO'] + '/')
-        return passthrough(request)
-    return appendslash
+        return self.passthrough(request)
